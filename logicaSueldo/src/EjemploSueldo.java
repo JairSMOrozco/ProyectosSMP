6fa9373a636c123e568a52d4ea97fa1f44850be1
JairOrozco <jairoztricker7@gmail.com>
2024-10-07 14:39:05 -0600

    Lógica para ejercicio calcular sueldo

---
+++ logicaSueldo/src/EjemploSueldo.java
@@ -0,0 +1,6 @@
+public class EjemploSueldo {
+    public static void main(String[] args) {
+        Sueldo mySalary = new Sueldo("Jair", 100, 13, 5);
+        System.out.println(mySalary.getWeeklySalary());
+    }
+}
---
+++ logicaSueldo/src/Sueldo.java
@@ -0,0 +1,60 @@
+public class Sueldo {
+    //Atributos
+    private String name;
+    private double salaryPerHour;
+    private double diaryHoursWorked;
+    private int daysWorked;
+    private double weeklySalary;
+
+    //Constructores
+
+    public Sueldo(String name, double salaryPerHour, double diaryHoursWorked, int daysWorked) {
+        this.name = name;
+        this.salaryPerHour = salaryPerHour;
+        this.diaryHoursWorked = diaryHoursWorked;
+        this.daysWorked = daysWorked;
+    }
+
+    //Getters
+    public double getWeeklySalary() {
+        salaryCalculator();
+        return this.weeklySalary;
+    }
+
+    //Métodos
+    private double calculator() {
+        return (this.salaryPerHour * this.diaryHoursWorked) * this.daysWorked;
+    }
+
+    public void salaryCalculator(){
+
+        //Si los días ingresados son mayores a 7, se asigna daysWorked = 7
+        if(this.daysWorked > 7){
+            this.daysWorked = 7;
+        }
+
+        //Según las horas trabajadas se hace el cálculo
+        /*if (this.diaryHoursWorked <= 8){
+            weeklySalary = calculator();
+        } else if (this.diaryHoursWorked > 8 && this.diaryHoursWorked <= 12) {
+            this.salaryPerHour *= 2;
+            weeklySalary = calculator();
+        }else if (this.diaryHoursWorked > 12){
+            this.salaryPerHour *= 3;
+            weeklySalary = calculator();
+        }*/
+
+        //Según las horas trabajadas se hace el cálculo
+        if (this.diaryHoursWorked <= 8){
+            weeklySalary = calculator();
+        } else if (this.diaryHoursWorked > 8 && this.diaryHoursWorked <= 12) {
+            double horasDiferencia = this.diaryHoursWorked - 8;
+            double pagoHorasExtra = horasDiferencia * (this.salaryPerHour * 2);
+            weeklySalary = ;
+        }else if (this.diaryHoursWorked > 12){
+            this.salaryPerHour *= 3;
+            weeklySalary = calculator();
+        }
+    }
+
+}
